33c6aa2eb73c01d6cbc4fbb2bb175b301f1add53
gin280 <zxy_wtfm@163.com>
2024-02-12 16:40:47 +0800

    change code

--- MyP.js
+++ MyP.js
@@ -1,72 +1,107 @@
-const airports = 'PHX BKK OKC JFK LAX MEX EZE HEL LOS LAP LIM'.split(' ')
+public class LookTrigger: MonoBehaviour {
+  [Range(0f, 1f)]
+  public float lookness = 0.5f;
 
-const routes = [
-  ['PHX', 'LAX'],
-  ['PHX', 'JFK'],
-  ['JFK', 'OKC'],
-  ['JFK', 'HEL'],
-  ['JFK', 'LOS'],
-  ['MEX', 'LAX'],
-  ['MEX', 'BKK'],
-  ['MEX', 'LIM'],
-  ['MEX', 'EZE'],
-  ['LIM', 'BKK'],
-]
+  public Transform objTf;
 
-const adjacencyList = new Map()
-
-function addNode(airport) {
-  adjacencyList.set(airport, [])
-}
-
-function addEdge(origin, destination) {
-  adjacencyList.get(origin).push(destination)
-  adjacencyList.get(destination).push(origin)
-}
-
-airports.forEach(addNode)
-routes.forEach((route) => addEdge(...route))
-
-function bfs(start, end) {
-  const visited = new Set()
-  const queue = [start]
-
-  while (queue.length > 0) {
-    const airport = queue.shift()
-    const destinations = adjacencyList.get(airport)
-
-    for (const destination of destinations) {
-      if (destination === end) {
-        console.log('found it')
-      }
-
-      if (!visited.has(destination)) {
-        visited.add(destination)
-        queue.push(destination)
-        console.log(destination)
-      }
-    }
+  void onDrawGizmos() {
+    Vector2 center = transform.position;
+    Vector2 playerPos = objTf.position
+    Vector2 playerLookDir = objTf.right;
+  
+    Vector2 playerToTriggerDir = (center - playerPos).normalized;
+  
+    float lookness = Vector2.Dot(playerLookDir, playerToTriggerDir);
+  
+    bool isLooking = lookness >= 0.5f;
+  
+    Gizmos.color = isLooking ? Color.green : Color.red
+    Gizmos.DrawLine(playerPos, playerToTriggerDir)
+    
+    Gizmos.color = Color.yellow
+    Gizmos.DrawLine(playerPos, playerPos + playerLookDir)
   }
 }
 
-// bfs('PHX', 'BKK')
+public class SpaceTransform: MonoBehaviour {
+  public Vector2 localSpacePoint
 
-function dfs(start, end, visited = new Set()) {
-  console.log(start)
-  visited.add(start)
+  void onDrawGizmos() {
 
-  const destinations = adjacencyList.get(start)
+    Vector2 objPos = transform.position;
+    Vector2 right = transform.right;
+    Vector2 up = transform.up;
 
-  for (const destination of destinations) {
-    if (destination === end) {
-      console.log(`found it in ${visited.size} steps!`)
-      return
+    Vector2 LocalToWorld(Vector2 localPoint) {
+      Vector2 localWorldOffset = right * localPoint.x + up * localPoint.y;
+      return (Vector2)transform.position + localWorldOffset;
     }
 
-    if (!visited.has(destination)) {
-      dfs(destination, end, visited)
-    }
+    Vector2 wordSpacePoint = LocalToWorld(localSpacePoint);
+
+    DrawBasisVectors(objPos, objRight, objUp);
+    DrawBasisVectors(Vector2.zero, Vector2.right, Vector2.up);
+
+    Gizmos.color = Color.blue;
+    Gizmos.DrawSphere(wordSpacePoint, .1f);
+
+  }
+
+ 
+
+  void DrawBasisVectors( Vector2 pos, Vector2 right, Vector2 up) {
+    Gizmos.color = Color.red;
+    Gizmos.DrawRay(pos, right);
+    Gizmos.color = Color.green;
+    Gizmos.DrawRay(pos, up);
+    Gizmos.color = Color.blue;
+    
   }
 }
 
-dfs('PHX', 'BKK')
+public class SpaceTransform: MonoBehaviour {
+  public Transform localObjTransform;
+  public Vector2 worldSpacePoint
+
+  void onDrawGizmos() {
+
+    Vector2 objPos = transform.position;
+    Vector2 right = transform.right;
+    Vector2 up = transform.up;
+
+    Vector2 LocalToWorld(Vector2 localPoint) {
+      Vector2 localWorldOffset = right * localPoint.x + up * localPoint.y;
+      return (Vector2)transform.position + localWorldOffset;
+    }
+
+    Vector2 WorldToLocal(Vector2 worldPt) {
+      Vector2 relPoint = worldPt - objPos
+
+      float x = Vector2.Dot(relPoint, right);
+      float y = Vector2.Dot(relPoint, up);
+
+      return new Vector2(x, y);
+    }
+
+
+    DrawBasisVectors(objPos, right, up);
+    DrawBasisVectors(Vector2.zero, Vector2.right, Vector2.up);
+
+    Gizmos.color = Color.cyan;
+    Gizmos.DrawSphere(wordSpacePoint, .1f);
+
+    localObjTransform.localPosition = WorldToLocal(worldSpacePoint);
+
+  }
+
+ 
+
+  void DrawBasisVectors( Vector2 pos, Vector2 right, Vector2 up) {
+    Gizmos.color = Color.red;
+    Gizmos.DrawRay(pos, right);
+    Gizmos.color = Color.green;
+    Gizmos.DrawRay(pos, up);
+    Gizmos.color = Color.blue;
+    
+  }
+}
